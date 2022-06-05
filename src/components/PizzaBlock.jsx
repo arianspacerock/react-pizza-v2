76549d7605e3e829be4eef7cac58ab97f7b34fc8
arianspacerock <siropemptyspace@gmail.com>
2022-06-05 18:59:08 +0300

    refactor: add key for type,size

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -16,12 +16,12 @@ function PizzaBlock({title, price, imageUrl, sizes, types}) {
             <div className="pizza-block__selector">
                 <ul>
                     {types.map((typeId) => (
-                        <li onClick={() => setActiveType(typeId)} className={activeType === typeId ? 'active' : ''}>{typesName[typeId]}</li>
+                        <li key{typeId} onClick={() => setActiveType(typeId)} className={activeType === typeId ? 'active' : ''}>{typesName[typeId]}</li>
                     ))}
                 </ul>
                 <ul>
                     {sizes.map((size, i) => (
-                        <li onClick={() => setActiveSize(i)} className={activeSize === i ? 'active' : ''}>{size} см.</li>
+                        <li key{size} onClick={() => setActiveSize(i)} className={activeSize === i ? 'active' : ''}>{size} см.</li>
                     ))}
                 </ul>
             </div>
