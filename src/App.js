52456b383dacfb852473558990de4f7770222591
arianspacerock <siropemptyspace@gmail.com>
2022-06-09 11:21:38 +0300

    refactor: add useEffect,useState for items(pizzas)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from "react";
 import './App.css';
 import './scss/app.scss';
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json"
 
 
 function App() {
-    fetch('https://629f7aa2461f8173e4ea8987.mockapi.io/items')
+    const [items, setItems] = React.useState([])
+
+    React.useEffect(() => {
+        fetch('https://629f7aa2461f8173e4ea8987.mockapi.io/items')
+            .then((res) => res.json())
+            .then((arr) => {
+                setItems(arr)
+            })
+    }, [])
+
 
     return (
         <div className="wrapper">
@@ -21,7 +30,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) => (
+                        {items.map((obj) => (
                             <PizzaBlock key={obj.id} {...obj}/>
                         ))}
                     </div>
