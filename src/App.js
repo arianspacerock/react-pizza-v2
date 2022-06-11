bf2268a8f756cdf287e90ac80a017331132079e7
arianspacerock <siropemptyspace@gmail.com>
2022-06-11 19:33:39 +0300

    refactor: add Skeleton for pizzas(isLoading), use 'fake array for skeleton'

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton";
+
 
 
 function App() {
     const [items, setItems] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
 
     React.useEffect(() => {
         fetch('https://629f7aa2461f8173e4ea8987.mockapi.io/items')
             .then((res) => res.json())
             .then((arr) => {
                 setItems(arr)
+                setIsLoading(false)
             })
     }, [])
 
@@ -30,9 +34,10 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {items.map((obj) => (
-                            <PizzaBlock key={obj.id} {...obj}/>
-                        ))}
+                        {isLoading
+                            ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+                            : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+                        }
                     </div>
                 </div>
             </div>
