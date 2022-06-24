8c225ef8bfd51be4d9bb6a421c209ee53bb7525a
arianspacerock <siropemptyspace@gmail.com>
2022-06-24 07:01:13 +0300

    refactor: add react router, add path for Cart, add Link for header, cart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './scss/app.scss';
 import {Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import NotFound from "./components/NotFoundBlock";
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
                 <div className="container">
                     <Routes>
                         <Route path="/" element={<Home/>}/>
-                        <Route path="not-found" element={<NotFound/>}/>
+                        <Route path="/cart" element={<Cart/>}/>
+                        <Route path="*" element={<NotFound/>}/>
                     </Routes>
                 </div>
             </div>
