56ae1975bc7138cf30594201bd7cb3fc0eb653d1
arianspacerock <siropemptyspace@gmail.com>
2022-07-26 23:16:52 +0300

    refactor: start add search for pizzas

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Search.module.scss"
+
+const Search = () => {
+    return (
+        <input className={styles.root} placeholder="Поиск пиццы ... "/>
+    )
+}
+
+export default Search
