4296bed157949906e125615e39944d14288e2120
arianspacerock <siropemptyspace@gmail.com>
2022-08-05 07:02:11 +0300

    refactor: add close for pizzas

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Search.module.scss"
 
-const Search = ({searhValue, setSearchValue}) => {
+const Search = ({searchValue, setSearchValue}) => {
     return (
         <div className={styles.root}>
             <svg
@@ -12,7 +12,18 @@ const Search = ({searhValue, setSearchValue}) => {
                         d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z"/>
                 </g>
             </svg>
-            <input className={styles.input} placeholder="Поиск пиццы ... "/>
+            <input
+                value={searchValue}
+                onChange={(event) => setSearchValue(event.target.value)}
+                className={styles.input}
+                placeholder="Поиск пиццы ... "
+            />
+                <svg className={styles.clearIcon} fill="none"  stroke="currentColor"
+                       viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <line x1="18" x2="6" y1="6" y2="18"/>
+                    <line x1="6" x2="18" y1="6" y2="18"/>
+                </svg>
         </div>
 
     )
