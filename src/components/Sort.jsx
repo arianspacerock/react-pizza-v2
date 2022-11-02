c89f2a6b8cd332120990ab7efa1ccb34df1bf0e3
arianspacerock <siropemptyspace@gmail.com>
2022-11-02 06:59:06 +0300

    refactor:  add hide sort on click

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -14,7 +14,7 @@ export const sortlist = [
 function Sort() {
     const dispatch = useDispatch()
     const sort = useSelector((state) => state.filter.sort)
-
+    const sortRef = React.useRef()
     const [open, setOpen] = React.useState(false)
 
     const onClickListItem = (obj) => {
@@ -22,8 +22,21 @@ function Sort() {
         setOpen(false)
     }
 
+    React.useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.path.includes(sortRef.current)) {
+                setOpen(false)
+            }
+        }
+
+        document.body.addEventListener('click', handleClickOutside)
+
+    }, [])
+
+
+
     return (
-        <div className="sort">
+        <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg
                     width="10"
