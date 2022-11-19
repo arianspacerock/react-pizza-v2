f6323c5453993ba6b594174b4ce8635ea676e93d
arianspacerock <siropemptyspace@gmail.com>
2022-11-19 17:21:44 +0300

    refactor: create FullPizza

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import {useParams} from "react-router-dom";
+import axios from "axios";
 
 const FullPizza = () => {
+    const [pizza, setPizza] = React.useState()
     const { id } = useParams()
+
+    React.useEffect(() => {
+        async function fetchPizza() {
+            try {
+                const { data } = await axios.get('https://629f7aa2461f8173e4ea8987.mockapi.io/items/' + id)
+                setPizza(data)
+            } catch (error) {
+                alert('Ошибка при получении пиццы!')
+            }
+        }
+        fetchPizza()
+    }, [])
 
     return (
         <div className="container">
-            <img src="" alt=""/>
-            <h2>{id}</h2>
-            <p>CATS</p>
-            <h4>250 ₽</h4>
+            <img src={pizza.imageUrl}/>
+            <h2>{pizza.title}</h2>
+            <h4>{pizza.price} ₽</h4>
         </div>
     )
 }
