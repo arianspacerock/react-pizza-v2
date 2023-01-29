34dbf43d6f82b07b0a264a976fb8e979527d5a80
arianspacerock <siropemptyspace@gmail.com>
2023-01-29 14:22:51 +0300

    refactor: start code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import './App.css';
 import './scss/app.scss';
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
 import NotFound from "./components/NotFoundBlock";
 import FullPizza from "./pages/FullPizza";
 import MainLayout from "./layouts/MainLayout";
+import React, {Suspense} from "react"
+
+const Cart = React.lazy(() => import('./pages/Cart'))
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<MainLayout/>}>
                 <Route path="" element={<Home/>}/>
-                <Route path="cart" element={<Cart/>}/>
-                <Route path="pizza/:id" element={<FullPizza/>}/>
+                <Route path="cart" element={
+                    <Suspense fallback={<div>Идет загрузка корзины...</div>}>
+                        <Cart/>
+                    </Suspense>}/>
+                <Route path="pizza/:id" element={
+                    <Suspense fallback={<div>Идет загрузка...</div>}>
+                        <FullPizza/>
+                    </Suspense>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Route>
         </Routes>
